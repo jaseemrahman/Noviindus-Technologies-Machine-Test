6bbd84610781adec8c10772762ca92ebc5b0bae5
Jaseem Rahman <jaseemrahmanjr@gmail.com>
2022-12-31 18:27:29 +0530

    forms.py correction

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,31 +1,15 @@
 #Import from the core django
-from django import forms
 from django.forms import ModelForm
 #Import from local app/library
 from django.contrib.auth.models import User
 
 
-# class LoginForm(forms.ModelForm):
-#       class Meta:
-#         model= User
-#         fields=  ['username' ,'password'  ]  
-
 class RegistreationForm(ModelForm):
       class Meta:
         model= User
         fields=  ['id', 'username', 'email', 'password' ,'first_name','last_name' ] 
-        # fields= '__all__'  
-               # this function will be used for the server side validation
-      def clean(self):
-          # data from the form is fetched using super function
-          super(RegistreationForm, self).clean()
-          # extract the fields ield from the data
-
-          username = self.cleaned_data['username']
-          password = self.cleaned_data['password']
-          print('test point')
-      
-          # return any errors if found.
-          return self.cleaned_data  
+      # def clean(self):
+      #     super(RegistreationForm, self).clean()
+      #     return self.cleaned_data  
 
 
